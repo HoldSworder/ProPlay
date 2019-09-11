4159bc3d76c56c39bd4014fe31df075c4148c026
Qzr <z5021996@vip.qq.com>
2019-09-11 18:07:22 +0800

    :construction: 继续天气组件

--- src/components/canvas/canvas-weather.vue
+++ src/components/canvas/canvas-weather.vue
@@ -9,7 +9,7 @@
 <script lang='ts'>
 import { Component, Vue } from "vue-property-decorator";
 import { canvasMixins, textMixins } from "@/mixins/canvas-element";
-import getCity from "@/common/city-code";
+// import getCity from "@/common/city-code";
 
 @Component({
   mixins: [canvasMixins, textMixins]
@@ -17,11 +17,29 @@ import getCity from "@/common/city-code";
 export default class Home extends Vue {
   data!: any;
   $axios!: any;
+  weatherText!: string;
   mounted() {
-    const cityCode = getCity("武汉");
-    this.$axios.get(`http://t.weather.sojson.com/api/weather/city/${cityCode}`).then(res => {
-      console.log(res)
-    });
+    // const cityCode = getCity("武汉");
+    this.$axios
+      .get(`http://wthrcdn.etouch.cn/weather_mini?city=${"武汉"}`)
+      .then((res: any) => {
+        const weatherData = res.data;
+        const forecast = weatherData.forecast[this.data.data];
+        this.weatherText = `
+          <p>${weatherData.city}</p>
+          <p>${forecast.date}</p>
+        `;
+
+        if (this.data.weather) {
+          this.weatherText += `<p>${forecast.type}</p>`;
+        } else if (this.data.temperature) {
+          this.weatherText += `
+          <p>${forecast.high}</p>
+          <p>${forecast.low}</p>`;
+        } else if (this.data.windPower) {
+          this.weatherText += `<p>${forecast.fengli.slice(9, -3)}</p>`;
+        }
+      });
   }
 }
 </script>
