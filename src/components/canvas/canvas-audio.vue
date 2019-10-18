217b918507d7a21e4b7bd7319e0a62cf081a24d8
Qzr <z5021996@vip.qq.com>
2019-10-18 17:45:57 +0800

    :construction: 添加视频音频设置开始结束时间功能

--- src/components/canvas/canvas-audio.vue
+++ src/components/canvas/canvas-audio.vue
@@ -1,19 +1,49 @@
 <template>
   <div class="canvas-container"
-       v-if="now >= start && now <= end && now >= 1">
+       v-if="now >= start && now <= end">
     <audio :src="require(`@/assets/${data.fileName}`)"
+           id="audio"
+           loop
            autoplay></audio>
   </div>
 </template>
 
 <script lang='ts'>
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Vue, Watch } from "vue-property-decorator";
 import { canvasMixins } from "@/mixins/canvas-element";
+import { formatToS } from "@/common/tool";
 
 @Component({
   mixins: [canvasMixins]
 })
-export default class audioCanvas extends Vue {}
+export default class audioCanvas extends Vue {
+  data!: any;
+  start!: number;
+  playType:boolean = false;
+  @Watch("now")
+  startPlay(val: number) {
+    this.$nextTick(function() {
+      if(this.playType) return
+      if (val > this.start) {
+        this.playType = true
+        this.setLoop();
+      }
+    });
+  }
+
+  //设置进出时间
+  setLoop() {
+    const THAT = this,
+      audio: any = document.querySelector("#audio"),
+      inTime: number = formatToS(this.data.inTime),
+      outTime: number = formatToS(this.data.outTime);
+    audio.currentTime = inTime;
+    console.log(audio.currentTime)
+    setTimeout(() => {
+      THAT.setLoop();
+    }, (outTime - inTime) * 1000);
+  }
+}
 </script>
 
 <style scoped>
--- src/components/canvas/canvas-video.vue
+++ src/components/canvas/canvas-video.vue
@@ -1,9 +1,11 @@
 <template>
   <transition :name="transitionName">
+    <!-- 视频加载延迟两秒 -->
     <div class="canvas-container"
-         v-if="now >= start && now <= end">
+         v-if="now >= (start - 2) && now <= end">
       <video :src="require(`@/assets/${data.fileName}`)"
              id="video"
+             loop
              autoplay
              :muted="!data.audio"
              :width="data.width"
@@ -16,21 +18,36 @@
 <script lang='ts'>
 import { Component, Vue, Watch } from "vue-property-decorator";
 import { canvasMixins } from "@/mixins/canvas-element";
+import { formatToS } from "@/common/tool";
 
 @Component({
   mixins: [canvasMixins]
 })
 export default class videoCanvas extends Vue {
+  data!: any;
+  start!: number;
+  playType:boolean = false;
   @Watch("now")
   startPlay(val: number) {
-    const THAT = this
     this.$nextTick(function() {
-      if (val == THAT.start) {
-        let video: any = document.querySelector("#video");
-        console.log(video);
-        video.currentTime = 20;
+      if(this.playType) return
+      if (val == this.start - 2) {
+        this.playType = true
+        this.setLoop();
       }
     });
+  }
+
+  //设置进出时间
+  setLoop() {
+    const THAT = this,
+      video: any = document.querySelector("#video"),
+      inTime = formatToS(this.data.inTime),
+      outTime = formatToS(this.data.outTime);
+    video.currentTime = inTime;
+    setTimeout(() => {
+      THAT.setLoop();
+    }, (outTime - inTime + 2) * 1000);
   }
 }
 </script>
