3b931c1ad01c920cdc59b11cbd3ea9c98b0cf466
Qzr <z5021996@vip.qq.com>
2019-10-25 18:07:44 +0800

    :construction: 修改播放功能 文字播放逻辑

--- src/components/canvas/canvas-text.vue
+++ src/components/canvas/canvas-text.vue
@@ -3,9 +3,12 @@
     <div v-if="now >= start && now <= end"
          class="canvas-container">
       <div ref="textContainer"
+           class="text-container"
+           :class="{isHor: isHor}"
            :style="{...elementStyle, ...textStyle}">
         <span class="text-box"
-              ref="textBox">{{data.text}}</span>
+              ref="textBox"
+              v-html="transText"></span>
       </div>
     </div>
   </transition>
@@ -22,61 +25,128 @@ import { textMixins } from "@/mixins/text-element";
 export default class textCanvas extends Vue {
   data!: any;
   start!: number;
+  isHor: boolean = true; //是否水平移动
+
+  get transText() {
+    const text = this.data.text;
+    // return JSON.parse(
+    //   JSON.stringify(text)
+    //     .replace(/\\n/g, "<br/>")
+    //     .replace(/ /g, "&nbsp;")
+    // );
+    let pText = text.replace(/<br\/>/g, "</p><p>");
+    return `<p>${pText}</p>`;
+  }
 
   @Watch("now")
   startPlay(val: number, old: number) {
     const THAT = this;
     if (val == this.start || (this.start == 0 && val == 1)) {
       console.log("ok");
-      let interval = setInterval(() => {
-        const box = THAT.$refs.textBox as HTMLElement;
-        const container = THAT.$refs.textContainer as HTMLElement;
-        if (!box) {
-          clearInterval(interval);
-          return;
-        }
-        let leftVal: number = parseInt(box.style.left as string) || 0;
-        let topVal: number = parseInt(box.style.top as string) || 0;
-        switch (THAT.data.rolling) {
-          case 0:
-            return;
-          case 1:
-            box.style.left = `${leftVal + THAT.data.playbackspeed}px`;
-            if (Math.abs(leftVal) > container.offsetWidth)
-              box.style.left = -box.offsetWidth + "px";
-            break;
-          case 2:
-            box.style.left = `${leftVal - THAT.data.playbackspeed}px`;
-            if (Math.abs(leftVal) > box.offsetWidth)
-              box.style.left = container.offsetWidth + "px";
-            break;
-          case 3:
-            box.style.top = `${topVal + THAT.data.playbackspeed}px`;
-            if (Math.abs(topVal) > container.offsetHeight)
-              box.style.top = -box.offsetHeight + "px";
-            break;
-          case 4:
-            box.style.top = `${topVal - THAT.data.playbackspeed}px`;
-            if (-topVal > box.offsetHeight)
-              box.style.top = container.offsetHeight + "px";
-            break;
-        }
+      const type = this.data.rolling;
+      let residencetime = this.data.residencetime;
 
-        const maxWidth = container.offsetWidth + box.offsetWidth;
-        const maxHeight = container.offsetHeight + box.offsetHeight;
-      }, THAT.data.residencetime);
+      if (type == 3 || type == 4) residencetime *= 1000;
+      else residencetime *= 10
+      
+      const interval = setInterval(
+        () => THAT.playText(interval),
+        residencetime
+      );
     }
+  }
+
+  playText(interval: any) {
+    const box = this.$refs.textBox as HTMLElement,
+      container = this.$refs.textContainer as HTMLElement,
+      leftVal: number = parseInt(box.style.left as string) || 0,
+      topVal: number = parseInt(box.style.top as string) || 0,
+      pHeight = window.getComputedStyle(box.children[0]).height,
+      containerH = container.offsetHeight;
+
+    if (!box) {
+      clearInterval(interval);
+      return;
+    }
+
+    switch (this.data.rolling) {
+      case 0:
+        return;
+      case 1:
+        box.style.left = `${leftVal + this.data.playbackspeed}px`;
+        if (Math.abs(leftVal) > container.offsetWidth) {
+          box.style.left = -box.offsetWidth + "px";
+        }
+        break;
+      case 2:
+        box.style.left = `${leftVal - this.data.playbackspeed}px`;
+        if (Math.abs(leftVal) > box.offsetWidth) {
+          box.style.left = container.offsetWidth + "px";
+        }
+        break;
+      case 3:
+        this.isHor = false;
+        box.style.top = `${topVal + containerH}px`;
+        if (Math.abs(topVal) > container.offsetHeight)
+          box.style.top = -box.offsetHeight + "px";
+        break;
+      case 4:
+        this.isHor = false;
+        box.style.top = `${topVal - containerH}px`;
+        if (-topVal > box.offsetHeight)
+          box.style.top = container.offsetHeight + "px";
+        break;
+      case 5:
+        box.style.top = `${topVal - this.data.playbackspeed}px`;
+        if (-topVal > box.offsetHeight) {
+          box.style.top = container.offsetHeight + "px";
+        }
+        break;
+    }
+
+    const maxWidth = container.offsetWidth + box.offsetWidth;
+    const maxHeight = container.offsetHeight + box.offsetHeight;
+  }
+
+  checkHor() {
+    //判断是否为水平移动
+    const box = this.$refs.textBox as HTMLElement,
+      container = this.$refs.textContainer as HTMLElement,
+      containerH = container.offsetHeight;
+
+    this.isHor = false;
+
+    if (this.data.rolling == 3) {
+      for (let i = 0; i < box.children.length; i++) {
+        const item = box.children[i] as HTMLElement;
+        item.style.lineHeight = containerH + "px";
+      }
+    }
+  }
+
+  mounted() {
+    this.checkHor();
   }
 }
 </script>
 
 <style scoped>
-.canvas-container div {
+.text-container {
   position: relative;
   overflow: hidden;
+}
+.isHor {
+  display: flex;
+  align-items: center;
 }
 .text-box {
   position: relative;
   left: 0;
+  display: flex;
+  flex-wrap: wrap;
+  transition: top 0.5s;
+}
+.text-box >>> p {
+  margin: 0;
 }
 </style>
